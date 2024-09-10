1466f424ad7e8e36d9985b0de3d22e689493b1df
Alejjaandro <alejandroolaso98@gmail.com>
2024-09-10 18:19:38 +0200

    login, logout and differentiate between users

--- Ejercicios/Ejercicio16/mi_web/src/proyecto/base/urls.py
+++ Ejercicios/Ejercicio16/mi_web/src/proyecto/base/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import ListaPendientes, DetalleTarea, CrearTarea, ActualizarTarea, EliminarTarea, EliminarTarea
+from .views import ListaPendientes, DetalleTarea, CrearTarea, ActualizarTarea, EliminarTarea, EliminarTarea, InicioSesion
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', ListaPendientes.as_view(), name='pendientes'),
+    path('iniciar-sesion/', InicioSesion.as_view(), name='iniciar_sesion'),
+    path('cerrar-sesion/', LogoutView.as_view(next_page='iniciar_sesion'), name='cerrar_sesion'),
     path('tarea/<int:pk>/', DetalleTarea.as_view(), name='detalle_tarea'),
     path('crear-tarea/', CrearTarea.as_view(), name='crear_tarea'),
     path('actualizar-tarea/<int:pk>/', ActualizarTarea.as_view(), name='actualizar_tarea'),
--- Ejercicios/Ejercicio16/mi_web/src/proyecto/base/views.py
+++ Ejercicios/Ejercicio16/mi_web/src/proyecto/base/views.py
@@ -5,34 +5,53 @@ from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Tarea
 
 # Create your views here.
-class ListaPendientes(ListView):
+class InicioSesion(LoginView):
+    template_name = 'iniciar_sesion.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('pendientes')
+
+class ListaPendientes(LoginRequiredMixin, ListView):
     model = Tarea
     template_name = 'pendientes.html'
     context_object_name = 'tareas'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tareas'] = context['tareas'].filter(usuario=self.request.user)
+        context['count'] = context['tareas'].filter(completada=False).count()
+        return context
 
-
-class DetalleTarea(DetailView):
+class DetalleTarea(LoginRequiredMixin, DetailView):
     model = Tarea
     template_name = 'detalle_tarea.html'
     context_object_name = 'tarea'
     
-class CrearTarea(CreateView):
+class CrearTarea(LoginRequiredMixin, CreateView):
     model = Tarea
     template_name = 'crear_tarea.html'
-    fields = '__all__'
+    fields = ['titulo', 'descripcion', 'completada']
     success_url = reverse_lazy('pendientes')
     
-class ActualizarTarea(UpdateView):
+    def form_valid(self, form):
+        form.instance.usuario = self.request.user
+        return super(CrearTarea, self).form_valid(form)
+    
+class ActualizarTarea(LoginRequiredMixin, UpdateView):
     model = Tarea
     template_name = 'crear_tarea.html'
     fields = '__all__'
     success_url = reverse_lazy('pendientes')
 
-class EliminarTarea(DeleteView):
+class EliminarTarea(LoginRequiredMixin, DeleteView):
     model = Tarea
     template_name = 'eliminar_tarea.html'
     context_object_name = 'tarea'
